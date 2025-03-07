d0e0c6d4c9349f8b8ee60fed620a926738c57f7f
Towhid <blatowhid@gmail.com>
2025-03-07 16:54:39 +0600

    Update 06_condition.jsx

--- 06_condition.jsx
+++ 06_condition.jsx
@@ -5,5 +5,6 @@ condition =>
           -directly use where need to 
           -avoid creating empty tags.
 => don't use condition inside a tag (when the tag can return emty value).
+=> ensure a value using condition then use it(like via condition i define color value red or green then use the variable where the color value should be)
 
 */
