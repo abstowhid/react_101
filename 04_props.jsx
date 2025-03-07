2a85932c88295fac3e9f0dacd9f5802cc20caf4b
Towhid <blatowhid@gmail.com>
2025-03-07 17:43:50 +0600

    Update 04_props.jsx

--- 04_props.jsx
+++ 04_props.jsx
@@ -4,3 +4,8 @@
             -dynamic vlaues
 dynamic styling => specific style values
 dynamic values => (like any kind of textcontent , or any kind of values,media etc)
+-------------------------
+way to use props =>
+            -manual define values
+            -based on condition auto change value
+            
