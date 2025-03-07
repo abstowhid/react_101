ed4930eba0e2b259ce1a0e40410f8a8de44ea245
Towhid <blatowhid@gmail.com>
2025-03-07 15:51:57 +0600

    Update condition.jsx

--- condition.jsx
+++ condition.jsx
@@ -2,6 +2,8 @@
 condition =>
           -terneray condition is usefull
           -declare in a variable >then re-use it
-          -directly use where need to
+          -directly use where need to 
           -avoid creating empty tags.
+=> don't use condition inside a tag (when the tag can return emty value).
+
 */
