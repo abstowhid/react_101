84c28d48ca7fcaabfb61a92b74767d21c621c5ff
Towhid <blatowhid@gmail.com>
2025-03-07 16:06:54 +0600

    Create 02_get-started.jsx

---
+++ 02_get-started.jsx
@@ -0,0 +1,23 @@
+// jsx 
+// {}
+// return
+// <> </>
+//
+
+
+
+/* 
+jsx=>
+  file format that allow to write both html and javascript code
+  
+{} =>
+  javaScript codes must be inlcued inside the {} bracket. specially inside return, when writing or passing javascript variabe , props >must use {}
+  
+return =>
+  include the DOM (the HTML codes) and only the Codes inside return is shown in the web.  reutrn can return only one parent element(like only one div and all emlements inside of the div, to solve this we have empty fragment)
+
+<> </>  =>
+  it's a empty fragment that solve the return problem. using this now we can use HTML as we want
+
+
+
