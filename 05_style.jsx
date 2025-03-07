81849f3dcebcb4d555ee990a9f0d3fc41bdf086f
Towhid <blatowhid@gmail.com>
2025-03-07 16:11:51 +0600

    Update 05_style.jsx

--- 05_style.jsx
+++ 05_style.jsx
@@ -4,4 +4,7 @@ style  =>
         -js variable object(object key=style property,object value=style value)
         -using props
 
+-using props =>
+        we can use props in the js variable (as key) and add(the variable) by style in HTML opennig tag ,then pass the value in the component as usual
+
 */
