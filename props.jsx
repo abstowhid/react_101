0a2646e8713b04cda3c86bd8e19764cb134cdc85
Towhid <blatowhid@gmail.com>
2025-03-07 15:41:34 +0600

    Create props.jsx

---
+++ props.jsx
@@ -0,0 +1,6 @@
+//property(props)
+/* props   =>
+            -dynamic styling
+            -dynamic vlaues
+dynamic styling => specific style values
+dynamic values => (like any kind of textcontent , or any kind of values,media etc)
