ac0e02d973ce8d507263de34c2e72bf2c068d3b5
Towhid <blatowhid@gmail.com>
2025-03-07 15:47:57 +0600

    Create condition.jsx

---
+++ condition.jsx
@@ -0,0 +1,7 @@
+/*
+condition =>
+          -terneray condition is usefull
+          -declare in a variable >then re-use it
+          -directly use where need to
+          -avoid creating empty tags.
+*/
