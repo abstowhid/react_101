e88635b10b394d65b38b67007a40b7be5dec7ae4
Towhid <blatowhid@gmail.com>
2025-03-18 15:36:12 +0600

    Update 07_event.jsx

--- 07_event.jsx
+++ 07_event.jsx
@@ -4,3 +4,22 @@ function Fname(event){
 event.preventDefault();
 }
 //this ensure to prevent default behaviour of event in react,(in facing problem then try it)
+
+
+//code example=>
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1); // Updating state inside a separate function
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
