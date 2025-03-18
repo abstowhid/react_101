1820f254acd3c7c7e14e128239f22e9abb42b03c
Towhid <blatowhid@gmail.com>
2025-03-18 15:35:45 +0600

    Update 07_event.jsx

--- 07_event.jsx
+++ 07_event.jsx
@@ -1,5 +1,5 @@
 //event
-//note: 
+//note: do not use () while callling. cz () it executes the function instatn. but we need only when the event happen then it will be called. so don' use ()
 function Fname(event){
 event.preventDefault();
 }
