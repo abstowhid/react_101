4f6629dd6b7d5e69249c3eeb0452f0a6c2886a31
Towhid <blatowhid@gmail.com>
2025-03-18 16:12:55 +0600

    Create 12_call-back.jsx

---
+++ 12_call-back.jsx
@@ -0,0 +1,16 @@
+// for best practice we need to call back functions properly so that we can use whenever however we want;
+function ThisIsAComponent(){
+  const[count , setCount]=useState(0); //setcount is a function
+function Fname(){
+    // instead => setCount(count+1);
+//  setCount(here goes the code or another func)
+  setCount((newCount)=>{newCount+1});  //as we know already here newCount store the "count" from the state(same for array,mapping overall key store)
+}
+return(
+  //here goes html
+  <button onclick={Fname}>Btn</button>  //no first bracket ().only when btn is clicke;
+)
+}
+
+// if we want a function will be executed instant the code refresh, thne we will use ()
+//if we want after a event it will execute only then we will not use ().
