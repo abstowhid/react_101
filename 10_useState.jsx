315bfff73ca5dbe26c8dbc44ef84a5f04dfe5b7b
Towhid <blatowhid@gmail.com>
2025-03-18 15:33:57 +0600

    Update and rename 010_useState.jsx to 10_useState.jsx

--- 10_useState.jsx
+++ 10_useState.jsx
@@ -9,3 +9,22 @@ Think of useState(0) as creating a box (state) with a default value (0).
 
     count is what’s inside the box.(the content inside the state)
     setCount is the tool to modify the contents of the box.(a function to update the content)
+
+
+//code example=>
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1); // Updating state inside a separate function
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
