3a2f70bac5da886d6b2e793fe22eea28adbb583c
Towhid <blatowhid@gmail.com>
2025-03-18 15:32:50 +0600

    Create 010_useState.jsx

---
+++ 010_useState.jsx
@@ -0,0 +1,11 @@
+const [count, setCount] = useState(0);
+//so=>
+useState => hook(useState hook) //that creates a state
+count =>state variable
+setCount => function.
+
+//here 
+Think of useState(0) as creating a box (state) with a default value (0).
+
+    count is what’s inside the box.(the content inside the state)
+    setCount is the tool to modify the contents of the box.(a function to update the content)
