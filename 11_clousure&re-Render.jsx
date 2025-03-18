ec63e6a966156500640d477cfbfbb67a8971b6c1
Towhid <blatowhid@gmail.com>
2025-03-18 15:45:54 +0600

    Update 11_clousure&re-Render.jsx

--- 11_clousure&re-Render.jsx
+++ 11_clousure&re-Render.jsx
@@ -1,1 +1,21 @@
+re-render =>
 
+  import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  console.log("Component re-rendered!"); // Runs every time React re-renders
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+//👉 Every time you click the button, setCount updates the state, and React re-renders the component to reflect the new count.
+//when major changes happen react re render the component to laod updated data.(like use state,props,parent component etc)(
+
+clousure=>
+  
