3ebcef7a72c5b922e2895fd3df1adb35d9f8e1ec
Towhid <blatowhid@gmail.com>
2025-03-18 16:26:58 +0600

    Update 12_call-back.jsx

--- 12_call-back.jsx
+++ 12_call-back.jsx
@@ -18,3 +18,23 @@ return(
 
 // if we want a function will be executed instant the code refresh, thne we will use ()
 //if we want after a event it will execute only then we will not use ().
+//again=>
+function Outer function(){
+let randomNumber=  Math.Random();
+  console.log('this should executes only once')
+}
+function ThisIsAComponent(){
+  
+  const[count , setCount]=useState(randomNumber); //we want only once when the refresh happen we will a random number and each time component refresh we don't want a new number(or we don't want that function to execute) so we are not using (). if we want each time component refresh it will execute again again we can use ().
+  //note:even the randomNumber function executes each time only the first execution will work as the furst random value is taken by useState then we make +1, that means the operation has started and is happening on the first number we chose, so even it executes again and again it is worthless and take extra data.so as we don't need that so don't use ()
+  function Fname(){
+  setCount((newCount)=>{
+   return newCount+1; 
+  }); 
+}
+
+return(
+  //here goes html
+  <button onclick={Fname}>Btn</button>  //this causes compononent re render(refresh)
+)
+}
