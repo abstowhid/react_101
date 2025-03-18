766c6158c4b24ee83bf9832e73bdd0364dbe08f3
Towhid <blatowhid@gmail.com>
2025-03-18 16:15:47 +0600

    Update 12_call-back.jsx

--- 12_call-back.jsx
+++ 12_call-back.jsx
@@ -1,13 +1,15 @@
 // for best practice we need to call back functions properly so that we can use whenever however we want;
 function ThisIsAComponent(){
   const[count , setCount]=useState(0); //setcount is a function
+  
 function Fname(){
-    // instead => setCount(count+1);
+ // instead => setCount(count+1);
 //  setCount(here goes the code or another func)
-  setCount((newCount)=>{
+  setCount((newCount)=>{   //as we know already here newCount store the "count" from the state(same for array,mapping overall key store)
    return newCount+1;  //as js function need return
-  });  //as we know already here newCount store the "count" from the state(same for array,mapping overall key store)
+  }); 
 }
+  
 return(
   //here goes html
   <button onclick={Fname}>Btn</button>  //no first bracket ().only when btn is clicke;
