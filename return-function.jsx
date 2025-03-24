f474a757c9b6dca270ed299ef75855f910a42e71
Towhid <blatowhid@gmail.com>
2025-03-24 14:19:06 +0600

    Update return-function.jsx

--- return-function.jsx
+++ return-function.jsx
@@ -4,4 +4,4 @@
 2. when not needded?
    =>while on arrow function with single line , here don't need return
 3. arrow function with {} curly bracker
-     => here need return must.. because {} means multiple
+     => here need return must.. because {} means multiple{specially in case object state}
