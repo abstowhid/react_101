2dd1ab791dfac703d1500949dbdafd2acd761836
Towhid <blatowhid@gmail.com>
2025-03-24 13:36:42 +0600

    Update 13_state-with-object.jsx

--- 13_state-with-object.jsx
+++ 13_state-with-object.jsx
@@ -1,1 +1,2 @@
 //object is used to do multiple related state operations using one state
+//means when i need to do same types of state , instead using multiple state use one state with object with multiple initialization i need, then use setState function o set each
