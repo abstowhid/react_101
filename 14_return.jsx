8fb50c15bfbb845ba90668dd72bb13fdb8edc3cf
Towhid <blatowhid@gmail.com>
2025-03-24 14:39:33 +0600

    Update 14_return.jsx

--- 14_return.jsx
+++ 14_return.jsx
@@ -5,3 +5,8 @@
    =>while on arrow function with single line , here don't need return
 3. arrow function with {} curly bracker
      => here need return must.. because {} means multiple{specially in case object state}
+4.return must be inside of the {} curly bracket of the function..
+like =>
+()=>{
+return {here code }  //for this curly {},we need return. not for the parent's {}.
+}
