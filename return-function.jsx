66e4df8eeb260fe2a40de5bf6ae2f1cabbe84096
Towhid <blatowhid@gmail.com>
2025-03-24 13:58:25 +0600

    Update return-function.jsx

--- return-function.jsx
+++ return-function.jsx
@@ -1,1 +1,5 @@
-in mapping,function or anywhere if need to return anything to show up in the UI ,, then return is must
+1.in mapping,function or anywhere if need to return anything to show up in the UI ,, then return is must
+
+
+2. when not needded?
+   =>while on arrow function with single line , here don't need return
