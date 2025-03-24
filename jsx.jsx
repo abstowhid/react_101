6828fb3e6efd4460add6f38da2674b8205460d5e
Towhid <blatowhid@gmail.com>
2025-03-24 12:27:03 +0600

    Create jsx.jsx

---
+++ jsx.jsx
@@ -0,0 +1,49 @@
+jsx allow to write markup language with javascript with some different rules and syntax (javascript XML)
+
+//jsx  vs HTML=>
+1.instead class="" we need to use className=""
+2.every other things will be in camel case
+3.inline style will be with javascript object(not as regular css)
+4.Only return one parent element.(so we use empty fragment)
+
+
+important about jsx=>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
